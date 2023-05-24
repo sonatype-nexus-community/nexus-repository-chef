9eea0dccbe4fb836398d33fde5927973b58debba
Jesper Lundqvist <j.lundqvist@criteo.com>
2023-05-24 20:41:48 +0200

    Fix search of metadata files during cookbook upload
    In rare cases a cookbook tarball can have additional
    metadata files belonging to another cookbook in their
    folder structure. We add a filter to just look for
    metadata files in either the root folder of the
    cookbook or one level down.

--- src/main/java/org/sonatype/nexus/repository/chef/internal/metadata/util/TgzParser.java
+++ src/main/java/org/sonatype/nexus/repository/chef/internal/metadata/util/TgzParser.java
@@ -18,11 +18,13 @@ public class TgzParser {
 
     @Nullable
     public InputStream getFileFromInputStream(final InputStream is, String fileName) throws IOException {
+        // Match the sought filename only if it is in the root of the archive or one subfolder down
+        String fileNameRegex = "^[^/]*[/]*" + fileName + "$";
         try (GzipCompressorInputStream gzis = new GzipCompressorInputStream(is)) {
             try (TarArchiveInputStream tais = new TarArchiveInputStream(gzis)) {
                 ArchiveEntry currentEntry;
                 while ((currentEntry = tais.getNextEntry()) != null) {
-                    if (currentEntry.getName().toLowerCase().endsWith(fileName)) {
+                    if (currentEntry.getName().toLowerCase().matches(fileNameRegex)) {
                         log.info(String.format("Found %s file in archive: %s", fileName, currentEntry.getName()));
                         byte[] buf = new byte[(int) currentEntry.getSize()];
                         tais.read(buf, 0, buf.length);
